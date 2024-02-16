a080bf92ae33a49bc42a3d92f1c3d37155f03862
ricardocarva <ricardofecarva@gmail.com>
2024-02-16 11:45:08 -0500

    Add romanToInt.py

--- Python/PalindromeNumber.py
+++ Python/PalindromeNumber.py
@@ -18,7 +18,7 @@
 
 # Input: x = 10
 # Output: false
-# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+# Explanation: Reads 01 fdrom right to left. Therefore it is not a palindrome.
  
 
 # Constraints:
