8a798477ad762852dcebde79b804a746353d946b
ricardocarva <ricardofecarva@gmail.com>
2024-02-22 14:21:57 -0500

    Included find first occurence of substring in string in Pyhton

---
+++ Python/FindIndexOfFirstOccurenceInString.py
@@ -0,0 +1,27 @@
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+# Example 1:
+
+# Input: haystack = "sadbutsad", needle = "sad"
+# Output: 0
+# Explanation: "sad" occurs at index 0 and 6.
+# The first occurrence is at index 0, so we return 0.
+# Example 2:
+
+# Input: haystack = "leetcode", needle = "leeto"
+# Output: -1
+# Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+# Constraints:
+
+# 1 <= haystack.length, needle.length <= 104
+# haystack and needle consist of only lowercase English characters.
+
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/?envType=featured-list&envId=top-amazon-questions?envType=featured-list&envId=top-amazon-questions
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
